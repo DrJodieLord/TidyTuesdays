8e8dd83e516cbe1c96b6334984090535ff09c211
jodie lord <jodielord@jodies-mbp.home>
2019-12-30 20:43:48 +0000

    Test script ready for first analysis

---
+++ 2020/FirstTidyTuesday/Rcode/Script1.R
@@ -0,0 +1,12 @@
+#################################
+#### TidyTuesday - Attempt 1 
+#### Jodie Lord
+#### 
+#################################
+
+
+# Loading libraries and datafiles ----------------------------------------------------------
+library(tidyverse)
+library(ggrepel)
+library(here)
+
